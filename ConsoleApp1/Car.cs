9c15a5566bf4ab355e750841a2dc8f5e5906b741
sveto <sveto@DESKTOP-97I2KID>
2022-02-03 17:53:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ namespace ConsoleApp1
+{
+    internal class Car : Vehicle
+    {
+        public override void Company()
+        {
+            Console.WriteLine("Car - Сompany");
+        }
+        public override void Type()
+        {
+            Console.WriteLine("Car - Type");
+        }
+        public void Racing()
+        {
+            Console.WriteLine("Car - Racing");
+        }
+        public void Model()
+        {
+            Console.WriteLine("Car - Model");
+        }
+        public void Price()
+        {
+            Console.WriteLine("Car- Price");
+        }
+    }
+}
